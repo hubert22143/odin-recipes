b67b5082c13c45fb37e929a875e4cdc15d344e82
Hubert Baciak <hubert.baciak@o2.pl>
2023-07-19 02:30:38 +0200

    Added js code, modified css/html code

---
+++ javascript.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () =>{
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+
+function update(bgColor,textColor){
+    html.style.backgroundColor = bgColor;
+    html.style.color = textColor;
+}
+
+select.addEventListener("change", () =>
+    select.value === "black"
+        ? update("black", "white")
+        : update("white", "black"),
+);
+});
